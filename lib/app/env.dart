24a83be3a11ff87b70c7403a2233c5dd92fa76ca
aldoulisescv <aldouli6@gmail.com>
2023-08-24 21:43:48 -0600

    intial commit

---
+++ lib/app/env.dart
@@ -0,0 +1,16 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Ultimate Salon Full App Flutter V2
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2023-present initappz.
+*/
+class Environments {
+  static const String appName = 'Ultimate Salon & Shop';
+  static const String companyName = 'Initappz';
+  static const String googleMapsKey = 'your-google-maps-key';
+  static const String apiBaseURL =
+      'https://your-api-url.com/public/';
+  static const String websiteURL = 'https://your-website-url.com/';
+}
